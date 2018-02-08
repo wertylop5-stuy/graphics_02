f00cf21b2e728daa4d9283adb0a5c5b598a95074
Stanley Lin <slin55055@gmail.com>
2018-02-08 11:01:40 -0500

    more setup

--- include/output.h
+++ include/output.h
@@ -7,4 +7,12 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 
+#include"draw.h"
+
+#define FILE_NAME	"picture.ppm"
+#define FILE_WIDTH	500
+#define FILE_HEIGHT	500
+
+void writeToFile(struct pixel **p);
+
 #endif
---
+++ output.c
@@ -0,0 +1,8 @@
+#include"include/output.h"
+
+void writeToFile(struct pixel **p) {
+	int fd = open(FILE_NAME, O_CREAT | O_WRONLY, 0644);
+	
+	for 
+}
+
