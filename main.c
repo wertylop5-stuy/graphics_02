847eedec7bea271692a4bc96c00bd27d8d04ccfe
Stanley Lin <slin55055@gmail.com>
2018-02-10 14:12:36 -0500

    cleanup

--- main.c
+++ main.c
@@ -1,37 +1,12 @@
 #include"include/draw.h"
 #include"include/output.h"
 
-//BUG: x bounds is messed up
-/*
-Observations:
-
-sizeof a row is 600
-sizeof struct pixel is 3
-600/3 is 200 (aka the width)
-
-[150][1663] is valid (null values)
-[0][31663] is valid the bounds
-[159][0] is invalid
-[158][0] is valid
-[158][63] is valid the bounds
-*/
-
 int main() {
 	Frame test;
 	memset(test, 0, sizeof(test));
 	
 	struct Pixel hi;
 	pixelColor(&hi, 255, 235, 205);
-	
-	//(10, 100) (10, 10)
-	//(0, 0) (10, 1)
-	//(20, 10) (30, 150)
-	//(20, 0) (30, 150)
-	//(0, 0) (200, 150)
-	/*
-	int x1 = 0, y1 = IMG_HEIGHT-1, x2 = 5, y2 = 0;
-	drawLine(test, x1, y1, x2, y2);
-	*/
 	
 	drawLine(test, 0, 0, 0, 100);
 	drawLine(test, 6, 100, 6, 0);
